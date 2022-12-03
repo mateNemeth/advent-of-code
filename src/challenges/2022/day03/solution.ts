3b41100232704c71a033358a823fcc7e0a27563b
Mate Nemeth <hello@matenemeth.hu>
2022-12-03 07:17:07 +0100

    day#3 part two

--- src/challenges/2022/day03/solution.ts
+++ src/challenges/2022/day03/solution.ts
@@ -9,17 +9,32 @@ const splitSacks = (str: string) => {
   return [str.slice(0, str.length / 2), str.slice(str.length / 2)];
 };
 
-let result = 0;
+let result_A = 0;
 data.forEach((sack) => {
   const [first, second] = splitSacks(sack);
   const firstMap = new Map();
   first.split('').forEach((letter) => firstMap.set(letter, letter));
   for (let i = 0; i < second.length; i++) {
     if (firstMap.has(second[i])) {
-      result += LETTERS.indexOf(second[i]) + 1;
+      result_A += LETTERS.indexOf(second[i]) + 1;
       break;
     }
   }
 });
 
-console.log(result);
+let result_B = 0;
+let cursor = 0;
+while (data.slice(cursor, cursor + 3).length) {
+  const group = data.slice(cursor, cursor + 3).sort((a, b) => a.length - b.length);
+  for (let i = 0; i < group[0].length; i++) {
+    const letter = group[0][i];
+    if (group[1].includes(letter) && group[2].includes(letter)) {
+      result_B += LETTERS.indexOf(group[0][i]) + 1;
+      break;
+    }
+  }
+  cursor += 3;
+}
+
+console.log(`Part A: ${result_A}`);
+console.log(`Part B: ${result_B}`);
